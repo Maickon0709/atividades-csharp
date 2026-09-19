3c3caca9785960fc4e01ef3793a561403c87c1bd
Maickon <maickon0729@gmail.com>
2026-09-19 10:53:02 -0300

    Adicionando a aula 14 e seus exercicios

---
+++ Aula14/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Aula14;
+
+public class Program
+{
+    public static void Main()
+    {
+        /*
+         for condição 1; condição 2; condição 3)
+        {
+             //Code que sera será executado
+        }
+
+        Declaração 1 é executada (uma unica vez) antes da execução do bloco de código.
+
+        Declaração 2 define a condição para executar o bloco de código.
+
+        Declaração 3 é executada (todas as vezes) após o bloco de código ter sido executado.
+
+        */
+
+        //Ex1:
+
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        //Ex2:
+
+        Console.WriteLine("==========Tabela de Multiplicação============");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.Write($"{i * j,6}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
